a04ccebea865f8e45a16506d63bc59d5263c83ee
liggia <liggia.cruz@gmail.com>
2021-08-23 12:05:31 -0300

    fundo mobile pronto

--- src/fundoMobile.js
+++ src/fundoMobile.js
@@ -2,12 +2,16 @@
 
 export default function FundoMobile(){
     return(
-    <div class="fundo-mobile">
-    <ion-icon name="home"></ion-icon>
-    <ion-icon name="search-outline"></ion-icon>
-    <ion-icon name="add-circle-outline"></ion-icon>
-    <ion-icon name="heart-outline"></ion-icon>
-    <ion-icon name="person-outline"></ion-icon>
+    <div class="fundo-mobile"> 
+      {namesIcon.map(nameIcon => <ion-icon name={nameIcon}></ion-icon>)}
   </div>
   );
 }
+
+const namesIcon = [
+  "home",
+  "search-outline",
+  "add-circle-outline",
+  "heart-outline",
+  "person-outline"
+]
