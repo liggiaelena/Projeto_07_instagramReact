177a84778a6ed0b1393fa0de4ce9cf4ac6fa7718
liggia <liggia.cruz@gmail.com>
2021-08-23 14:36:29 -0300

    post finalizado

--- src/PartesDoCorpo/posts.js
+++ src/PartesDoCorpo/posts.js
@@ -1,77 +1,71 @@
 export default function Posts(){
     return(
         <div class="posts">
-            <div class="post">
-            <div class="topo">
-                <div class="usuario">
-                <img src="assets/img/meowed.svg" />
-                meowed
-                </div>
-                <div class="acoes">
-                <ion-icon name="ellipsis-horizontal"></ion-icon>
-                </div>
-            </div>
-
-            <div class="conteudo">
-                <img src="assets/img/gato-telefone.svg" />
-            </div>
-
-            <div class="fundo">
-                <div class="acoes">
-                <div>
-                    <ion-icon name="heart-outline"></ion-icon>
-                    <ion-icon name="chatbubble-outline"></ion-icon>
-                    <ion-icon name="paper-plane-outline"></ion-icon>
-                </div>
-                <div>
-                    <ion-icon name="bookmark-outline"></ion-icon>
-                </div>
-                </div>
-
-                <div class="curtidas">
-                <img src="assets/img/respondeai.svg" />
-                <div class="texto">
-                    Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
-                </div>
-                </div>
-            </div>
-            </div>
-
-            <div class="post">
-            <div class="topo">
-                <div class="usuario">
-                <img src="assets/img/barked.svg" />
-                barked
-                </div>
-                <div class="acoes">
-                <ion-icon name="ellipsis-horizontal"></ion-icon>
-                </div>
-            </div>
-
-            <div class="conteudo">
-                <img src="assets/img/dog.svg" />
-            </div>
-
-            <div class="fundo">
-                <div class="acoes">
-                <div>
-                    <ion-icon name="heart-outline"></ion-icon>
-                    <ion-icon name="chatbubble-outline"></ion-icon>
-                    <ion-icon name="paper-plane-outline"></ion-icon>
-                </div>
-                <div>
-                    <ion-icon name="bookmark-outline"></ion-icon>
-                </div>
-                </div>
-
-                <div class="curtidas">
-                <img src="assets/img/adorable_animals.svg" />
-                <div class="texto">
-                    Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
-                </div>
-                </div>
-            </div>
-            </div>
+            {infoPost.map(Post)}
         </div>
     );
 }
+
+
+function Post(info) {
+    return(
+        <div class="post">
+            <div class="topo">
+                <div class="usuario">
+                <img src={`assets/img/${info.nameUser}.svg`}/>
+                {info.nameUser}
+                </div>
+                <div class="acoes">
+                    <ion-icon name="ellipsis-horizontal"></ion-icon>
+                </div>
+            </div>
+            <div class="conteudo">
+                <img src={info.conteudo} />
+            </div>
+            <div class="fundo">
+                <div class="acoes">
+                    <div>
+                    {namesIcon.map(nameIcon => <ion-icon name={nameIcon}></ion-icon>)}
+                    </div>
+                    <div>
+                    <ion-icon name="bookmark-outline"></ion-icon>
+                    </div>
+                </div>
+
+                <div class="curtidas">
+                    <img src={info.img} />
+                    <div class="texto">
+                        Curtido por <strong>{info.seguidor}</strong> e <strong>outras {info.curtidas} pessoas</strong>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    );
+}
+
+
+const namesIcon = [
+    "heart-outline",
+    "chatbubble-outline",
+    "paper-plane-outline"
+
+]
+
+const infoPost =[
+    {
+        nameUser:'meowed',
+        img:'assets/img/respondeai.svg',
+        conteudo:'assets/img/gato-telefone.svg',
+        seguidor:'respondeai',
+        curtidas:'101.523'
+    },
+    {
+        nameUser:'barked',
+        img:'assets/img/adorable_animals.svg ',
+        conteudo:'assets/img/dog.svg',
+        seguidor:'adorable_animals',
+        curtidas:'99.159'
+    }
+    
+]
